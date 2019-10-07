656568ff0797ffd091e1dd4baaab9b67b252f38e
chambrig <54903181+chambrig@users.noreply.github.com>
2019-10-07 19:25:32 -0400

    Update js functions.js

---
+++ Lab 5/js functions.js
@@ -0,0 +1,25 @@
+function loopCountries(){
+	sessionStorage.setItem("countries", JSON.stringify(countries));
+
+	const countryStr = sessionStorage.getItem("countries");
+	const countryJson = JSON.parse(countryStr); 
+
+	let list = document.getElementsByClassName("instructions")[0];
+	list.style.listStyle = "decimal";
+	list.innerHTML = "";
+
+	//make a deep copy so I can remove countries
+	let randomArray = JSON.parse(JSON.stringify(countryJson));
+
+	for(i = 0; i < 50; i++){
+		let randomIndex = Math.floor(Math.random()*randomArray.length)
+		let countryNode = document.createElement("LI");
+		let countryName = document.createTextNode(randomArray[randomIndex].name);
+		randomArray.splice(randomIndex, 1);
+		countryNode.appendChild(countryName);
+		list.appendChild(countryNode);
+	}
+}
+
+document.getElementsByClassName("button")[0].addEventListener("click", loopCountries)
+
